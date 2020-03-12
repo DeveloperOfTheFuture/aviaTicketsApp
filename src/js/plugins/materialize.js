65f2c9a955f5a8b1602baa16164931baec997328
ne ponyal <kretov.il@mail.ru>
2020-03-12 23:19:32 +0300

    Если вкратце, то: добавил автозаполнение, передачу запроса и ответ сервера по свободным билетам

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -29,7 +29,8 @@ export function getAutocompleteInstance(elem) {
 
 const datepickers = document.querySelectorAll('.datepicker');
 M.Datepicker.init(datepickers, {
-  showClearBtn: true
+  showClearBtn: true,
+  format: 'yyyy-mm'
 });
 
 export function getDatePickerInstance(elem) {
--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -5,6 +5,7 @@ class Api {
   constructor(config) {
     this.url = config.url;
   }
+
   async countries() {
     try {
       const response = await axios.get(`${this.url}/countries`);
@@ -14,9 +15,22 @@ class Api {
       return Promise.reject(e);
     }
   }
+
   async cities() {
     try {
       const response = await axios.get(`${this.url}/cities`);
+      return response.data;
+    } catch (e) {
+      console.log(e);
+      return Promise.reject(e);
+    }
+  }
+
+  async prices(params) {
+    try {
+      const response = await axios.get(`${this.url}/prices/cheap`, {
+        params
+      });
       return response.data;
     } catch (e) {
       console.log(e);
