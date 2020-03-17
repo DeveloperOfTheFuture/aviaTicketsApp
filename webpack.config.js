f541ba2f26fd54dff67c94019da9594e5b51f7bf
ne ponyal <kretov.il@mail.ru>
2020-03-17 09:12:41 +0300

    В конфиг webpack добавил плагин очистки; собрал build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -63,6 +66,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: './style.css'
     }),
